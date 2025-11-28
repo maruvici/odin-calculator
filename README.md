7ba6c6c55e286262e2dd72565bc9ef05fac0c3c7
Mav <196895938+maruvici@users.noreply.github.com>
2025-11-28 20:03:46 +0800

    fix: Fix sequential operation press error
    Similar issue as previous commit. Pressing two
    operations in succession caused op1 (operation)
    op1. Solved by adding opFlag.

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # odin-calculator
 
-Repository made for The Odin Project - Foundations. A simple web calculator capable of
-basic arithmetic operations with integers and floats. Capstone project of TOP-Foundations.
+Repository made for The Odin Project - Foundations. A simple web calculator capable of basic arithmetic operations with integers and floats. Capstone project of TOP-Foundations.
 
 Skills-in-Practice
 
--- scripts/script.js
+++ scripts/script.js
@@ -6,8 +6,8 @@ const deleteButton = document.querySelector("#delete");
 const equalButton = document.querySelector("#equal");
 const logs = document.querySelector(".logs");
 const screen = document.querySelector(".calculator-screen");
-let op1 = (op2 = op = null);
-let appendMode = false;
+let op1 = (op2 = op = ans = null);
+let appendMode = (opFlag = false);
 
 function add(a, b) {
   return +a + +b;
@@ -23,16 +23,15 @@ function multiply(a, b) {
 
 function divide(a, b) {
   if (b == 0) {
-    return "ERROR";
+    return "IMPOSSIBLE";
   }
   return +a / +b;
 }
 
-function addHistory(op1, op2, op) {
+function addHistory(op1, op2, op, ans) {
   const log = document.createElement("div");
   log.classList.add("log");
-  const result = screen.textContent;
-  const logText = document.createTextNode(`${op1} ${op} ${op2} = ${result}`);
+  const logText = document.createTextNode(`${op1} ${op} ${op2} = ${ans}`);
   log.appendChild(logText);
   logs.appendChild(log);
 }
@@ -50,78 +49,132 @@ function operate(op1, op2, op) {
   }
 }
 
-// DISPLAY NUMBERS ON SCREEN
-numButtons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    if (appendMode) {
-      screen.textContent += e.target.id;
-    } else {
-      screen.textContent = e.target.id;
-      appendMode = true;
-    }
-  });
-});
+function reset() {
+  op1 = op2 = op = ans = null;
+  appendMode = opFlag = false;
+}
 
-// HANDLE OPERATIONS
-opButtons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    // Protect against non-numeric operands
-    if (!isNaN(screen.textContent)) {
-      op1 = screen.textContent;
-      op = e.target.id;
-      appendMode = false;
-    } else {
-      screen.textContent = "Invalid Operand";
-    }
-  });
-});
+function displayNumber(num) {
+  if (appendMode && screen.textContent !== "0") {
+    screen.textContent += num;
+  } else {
+    screen.textContent = num;
+    appendMode = true;
+  }
+  opFlag = false;
+}
 
-// DISPLAY ANSWER AND LOG CALCULATION IN HISTORY
-equalButton.addEventListener("click", (e) => {
+function pickOperation(operation) {
+  if (!opFlag) {
+    opFlag = true;
+    // If there is a chained operation
+    if (op) {
+      // Check if there is an error
+      if (isNaN(op1)) {
+        screen.textContent = "Invalid Operand";
+        reset();
+      } else {
+        op2 = screen.textContent;
+        let ans = operate(op1, op2, op);
+        if (isNaN(ans)) {
+          screen.textContent = ans; // For errors
+        } else {
+          screen.textContent = Math.round(ans * 1000) / 1000;
+        }
+        addHistory(op1, op2, op, ans);
+        if (isNaN(ans)) {
+          op1 = null;
+        } else {
+          op1 = ans;
+        }
+        op2 = null;
+        op = operation;
+        appendMode = false;
+      }
+    } else {
+      // Check if there is an error
+      if (isNaN(screen.textContent)) {
+        screen.textContent = "Invalid Operand";
+        reset();
+      } else {
+        op1 = screen.textContent;
+        op = operation;
+        appendMode = false;
+      }
+    }
+  }
+}
+
+function equate() {
   // To finalize operation, 3 conditions must be met:
   // 1. Operand 1 exists
   // 2. Operation exists
   // 3. Op2 is a NEW number on screen (implied by appendMode)
   if (op1 && op && appendMode) {
     op2 = screen.textContent;
-    let ans = operate(op1, op2, op);
+    ans = operate(op1, op2, op);
+    addHistory(op1, op2, op, ans);
     if (isNaN(ans)) {
       screen.textContent = ans; // For errors
+      reset();
     } else {
       screen.textContent = Math.round(ans * 1000) / 1000;
+      // If next button press == numButton: op1 = newNumber
+      // Else: op1 = ans (on-screen)
+      op2 = op = null;
+      appendMode = opFlag = false;
     }
-    addHistory(op1, op2, op);
-
-    // If next button press == numButton: op1 = newNumber
-    // Else: op1 = ans (on-screen)
-    op2 = op = null;
-    appendMode = false;
   }
-});
+}
 
-// ADD DECIMAL POINT
-decimalButton.addEventListener("click", (e) => {
+function addDecimalPoint() {
   if (!screen.textContent.includes(".")) {
     screen.textContent += ".";
   }
-});
+}
 
-// DELETE LAST SYMBOL ON SCREEN
-deleteButton.addEventListener("click", (e) => {
+function deleteNumber() {
   let textLength = screen.textContent.length;
   if (textLength > 1) {
     screen.textContent = screen.textContent.substring(0, textLength - 1);
   } else {
     screen.textContent = "0";
   }
-});
+}
 
-// RESET EVERYTHING
-clearButton.addEventListener("click", (e) => {
+function clearScreen() {
   op1 = op2 = op = null;
   screen.textContent = "0";
-  appendMode = false;
+  appendMode = opFlag = false;
   while (logs.firstChild) {
     logs.removeChild(logs.lastChild);
   }
+}
+
+// EVENT LISTENERS
+
+// DISPLAY NUMBERS ON SCREEN
+numButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    displayNumber(e.target.id);
+  });
 });
+
+// HANDLE OPERATIONS
+opButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    pickOperation(e.target.id);
+  });
+});
+
+// DISPLAY ANSWER AND LOG CALCULATION IN HISTORY
+equalButton.addEventListener("click", equate);
+
+// ADD DECIMAL POINT
+decimalButton.addEventListener("click", addDecimalPoint);
+
+// DELETE LAST SYMBOL ON SCREEN
+deleteButton.addEventListener("click", deleteNumber);
+
+// RESET EVERYTHING
+clearButton.addEventListener("click", clearScreen);
